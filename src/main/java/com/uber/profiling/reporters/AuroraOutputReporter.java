aca37a6d25a147aaadd176b25c26351a3a89ff0a
John Rice <john.rice@arcadia.io>
2020-11-22 21:27:21 -0500

    Store stacktraces

--- src/main/java/com/uber/profiling/reporters/AuroraOutputReporter.java
+++ src/main/java/com/uber/profiling/reporters/AuroraOutputReporter.java
@@ -1,6 +1,7 @@
 package com.uber.profiling.reporters;
 
 import com.uber.profiling.Reporter;
+import com.uber.profiling.util.JsonUtils;
 
 import java.sql.*;
 import java.util.Map;
@@ -12,32 +13,41 @@ public class AuroraOutputReporter implements Reporter {
 
     private final String FUNNY_PASSWORD = "password";
 
-
     public void report(String profilerName, Map<String, Object> metrics) {
-        System.out.println("report()");
-        doStuff();
+        if (metrics.containsKey("stacktrace")) {
+            System.out.println("AuroraOutputReporter(): Storing stacktrace.");
+            String startEpoch = metrics.get("startEpoch").toString();
+            String endEpoch = metrics.get("endEpoch").toString();
+            String stackTraceRaw = JsonUtils.serialize(metrics);
+            storeStacktrace(startEpoch, endEpoch, stackTraceRaw);
+        }
+
     }
 
     public void close() {
         System.out.println("close()");
     }
 
-    public void doStuff() {
+    public void storeStacktrace(String startEpoch, String endEpoch, String stackTraceRaw) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException ex) {
-            System.out.println("jdbc driver not found");
+            System.out.println("AuroraOutputReporter(): jdbc driver not found");
         }
         try {
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/", "root", FUNNY_PASSWORD);
 
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from emp");
-            while (rs.next())
-                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/timeseries_profiler_data", "root", FUNNY_PASSWORD);
+
+            String insert = String.format("INSERT INTO stacktrace (timestamps, stacktrace_raw) VALUES (?, ?)");
+            PreparedStatement ps = con.prepareStatement(insert);
+            ps.setString(1, startEpoch + "." + endEpoch);
+            ps.setString(2, stackTraceRaw);
+
+            int res = ps.executeUpdate();
             con.close();
         } catch (SQLException ex) {
+            System.out.println("AuroraOutputReporter(): SQLException thrown.");
             System.out.println(ex.getMessage());
         }
     }
