96d9dcf069e1cfe95e20611b81b3d72c6d5a50df
John Rice <john.rice@arcadia.io>
2020-11-23 13:16:47 -0500

    Connection pooling

--- src/main/java/com/uber/profiling/reporters/AuroraOutputReporter.java
+++ src/main/java/com/uber/profiling/reporters/AuroraOutputReporter.java
@@ -4,10 +4,17 @@ import com.uber.profiling.Reporter;
 import com.uber.profiling.util.JsonUtils;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.*;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class AuroraOutputReporter implements Reporter {
+
+    private static ConcurrentLinkedDeque<Connection> connPool;
 
     public AuroraOutputReporter() {
     }
@@ -15,13 +22,14 @@ public class AuroraOutputReporter implements Reporter {
     private final String FUNNY_PASSWORD = "password";
 
     public void report(String profilerName, Map<String, Object> metrics) {
-     /*   if (metrics.containsKey("stacktrace")) {
+        if (connPool == null) initConnectionPool();
+        if (metrics.containsKey("stacktrace")) {
             System.out.println("AuroraOutputReporter(): Storing stacktrace.");
             String startEpoch = metrics.get("startEpoch").toString();
             String endEpoch = metrics.get("endEpoch").toString();
             String stackTraceRaw = JsonUtils.serialize(metrics);
             storeStacktrace(startEpoch, endEpoch, stackTraceRaw.trim());
-        }*/
+        }
         if (profilerName.equalsIgnoreCase("cpuandmemory")) {
             System.out.println("AuroraOutputReporter(): Storing CPU and memory stats.");
             storeCpuAndMemory(metrics);
@@ -30,32 +38,54 @@ public class AuroraOutputReporter implements Reporter {
 
     }
 
-    public void close() {
-        System.out.println("close()");
-    }
-
-    public void storeStacktrace(String startEpoch, String endEpoch, String stackTraceRaw) {
+    private void initConnectionPool() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException ex) {
             System.out.println("AuroraOutputReporter(): jdbc driver not found");
         }
+        connPool = new ConcurrentLinkedDeque<>();
         try {
+            for (int i = 0; i < 5; i++) {
+                connPool.push(DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/timeseries_profiler_data", "root", FUNNY_PASSWORD));
+            }
+        } catch (SQLException ex) {
+            System.out.println("AuroraOutputReporter(): couldn't init pool.");
+            System.out.print(ex.getMessage());
+        }
+    }
 
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/timeseries_profiler_data", "root", FUNNY_PASSWORD);
+    public void close() {
+        try {
+            for (Connection c : connPool) {
+                c.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println("couldnt close");
+        }
+    }
+
+    public void storeStacktrace(String startEpoch, String endEpoch, String stackTraceRaw) {
+
+        try {
+            while (connPool.isEmpty()) {
+                Thread.sleep(5000);
+            }
+            Connection con = connPool.pop();
 
             String insert = String.format("INSERT INTO stacktrace (timestamps, stacktrace_raw) VALUES (?, ?)");
             PreparedStatement ps = con.prepareStatement(insert);
             ps.setString(1, startEpoch + "." + endEpoch);
             ps.setString(2, stackTraceRaw);
 
-            System.out.println("stackTraceRaw:\n" + stackTraceRaw);
-
             int res = ps.executeUpdate();
-            con.close();
+            connPool.push(con);
         } catch (SQLException ex) {
             System.out.println("AuroraOutputReporter(): SQLException thrown.");
+            System.out.println(ex.getMessage());
+        } catch (InterruptedException ex) {
+            System.out.print("InterruptedException");
             System.out.println(ex.getMessage());
         }
     }
@@ -69,40 +99,39 @@ public class AuroraOutputReporter implements Reporter {
         String nonheap_memory_committed_in_jvm = metrics.get("nonHeapMemoryCommitted").toString();
         String heap_memory_in_use   = metrics.get("heapMemoryTotalUsed").toString();
         String heap_memory_committed_in_jvm = metrics.get("heapMemoryCommitted").toString();
+        String host = metrics.get("name").toString();
 
 
         BigDecimal total_memory_in_use = new BigDecimal(new BigDecimal(heap_memory_in_use).intValue() + new BigDecimal(nonheap_memory_in_use).intValue());
         BigDecimal total_memory_committed = new BigDecimal(new BigDecimal(heap_memory_committed_in_jvm).intValue() + new BigDecimal(nonheap_memory_committed_in_jvm).intValue());
 
-
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException ex) {
-            System.out.println("AuroraOutputReporter(): jdbc driver not found");
-        }
-        try {
-
-            Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/timeseries_profiler_data", "root", FUNNY_PASSWORD);
+            while (connPool.isEmpty()) {
+                Thread.sleep(5000);
+            }
+            Connection con = connPool.pop();
 
             String insert = String.format("INSERT INTO cpuandmemory " +
-                    "(transform_name, timestamp_epoch_ms, system_cpu_usage, process_cpu_usage, memory_in_use, memory_committed_to_jvm)" +
-                    "VALUES (?,?,?,?,?,?)");
-
+                    "(transform_name, timestamp_epoch_ms, system_cpu_usage, process_cpu_usage, memory_in_use, memory_committed_to_jvm, host)" +
+                    "VALUES (?,?,?,?,?,?,?)");
 
 
             PreparedStatement ps = con.prepareStatement(insert);
             ps.setString(1, transform_name);
-            ps.setInt(2, Integer.valueOf(timestamp));
+            ps.setBigDecimal(2, new BigDecimal(timestamp));
             ps.setBigDecimal(3, new BigDecimal(system_cpu_usage));
             ps.setBigDecimal(4, new BigDecimal((process_cpu_usage)));
             ps.setInt(5, total_memory_in_use.intValue());
             ps.setInt(6, total_memory_committed.intValue());
+            ps.setString(7, host);
 
             int res = ps.executeUpdate();
-            con.close();
+            connPool.push(con);
         } catch (SQLException ex) {
             System.out.println("AuroraOutputReporter(): SQLException thrown.");
+            System.out.println(ex.getMessage());
+        } catch (InterruptedException ex) {
+            System.out.print("InterruptedException");
             System.out.println(ex.getMessage());
         }
     }
