555258c0702edccdbb5b659b03ded7b3b5eb707a
John Rice <john.rice@arcadia.io>
2021-03-26 12:21:43 -0400

    list

--- src/main/java/com/uber/profiling/reporters/S3OutputReporter.java
+++ src/main/java/com/uber/profiling/reporters/S3OutputReporter.java
@@ -3,10 +3,13 @@ package com.uber.profiling.reporters;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.uber.profiling.Reporter;
 import com.uber.profiling.util.JsonUtils;
 
+import java.util.List;
 import java.util.Map;
 
 public class S3OutputReporter implements Reporter {
@@ -15,10 +18,17 @@ public class S3OutputReporter implements Reporter {
     public void report(String profilerName, Map<String, Object> metrics) {
         System.out.println("S3OutputReporter report()");
         String json = JsonUtils.serialize(metrics);
-        String path =  "cigna.data.implementation2.arcadia/bugathon/";
+        String path = "cigna.data.implementation2.arcadia/bugathon/sources/macig/instances/bugathon/events/jvm_metrics_output/";
+        //String path =  "cigna.data.implementation2.arcadia/bugathon/";
         String key = "metrics.txt";
 
         try {
+            System.out.println("Doing a LIST on events.");
+            List<S3ObjectSummary> o = s3Client.listObjects("cigna.data.implementation2.arcadia/bugathon/sources/macig/instances/bugathon/events/").getObjectSummaries();
+            System.out.println("number of objects found: " + o.size());
+            for(S3ObjectSummary s : o) {
+                System.out.println("summary: " + s.getKey());
+            }
             System.out.println("Writing to S3: " + json);
             PutObjectResult res = s3Client.putObject(path, key, json);
             System.out.println("S3 client returned from putObject().");
