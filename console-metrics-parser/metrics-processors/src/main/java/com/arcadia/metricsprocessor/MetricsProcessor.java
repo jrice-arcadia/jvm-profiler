57655b62d5c010947b8ec816ee509ead5ebf0f08
John Rice <john.rice@arcadia.io>
2021-04-01 21:45:13 -0400

    Writes CPU and memory to CSV.

--- console-metrics-parser/metrics-processors/src/main/java/com/arcadia/metricsprocessor/MetricsProcessor.java
+++ console-metrics-parser/metrics-processors/src/main/java/com/arcadia/metricsprocessor/MetricsProcessor.java
@@ -6,6 +6,7 @@ import java.io.*;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -26,7 +27,7 @@ public class MetricsProcessor {
     private static Gson g;
     private static String PARENT_DIR = "";
     private static StringBuffer output; // thread safe
-    private static final String OUTPUT_FILENAME = "metrics.txt";
+    private static final String OUTPUT_FILENAME = "metrics.csv";
 
     public static void main(String... args) throws Exception{
         String path = args[0];
@@ -47,7 +48,10 @@ public class MetricsProcessor {
         CPU_AND_MEM_SCHEMA.add("memory_committed");
         CPU_AND_MEM_SCHEMA.add("virtmemsize_incl_swap");
         CPU_AND_MEM_SCHEMA.add("max_memory_available");
-        CPU_AND_MEM_SCHEMA.add("virt_mem_hwm,mem_usage");
+        CPU_AND_MEM_SCHEMA.add("virt_mem_hwm");
+        CPU_AND_MEM_SCHEMA.add("mem_usage");
+
+        output.append(String.join(",", CPU_AND_MEM_SCHEMA) + "\n");
 
         Stream<Path> metricsFiles = Files.list(Paths.get(path));
         metricsFiles.forEach(p ->  {
@@ -58,17 +62,21 @@ public class MetricsProcessor {
                 String taskId = tmp[tmp.length-2];
                 Stream<String> s = Files.lines(p);
                 String[] lines = Files.lines(p).toArray(String[]::new); // this will be small.
-                s.filter(l -> l.contains("CpuAndMemory") || l.contains("heapmax"))
+                s.filter(l -> l.contains("CpuAndMemory") || l.contains("heapMemoryMax"))
                         .forEach(l -> processLine(l, taskId));
                 s.close();
             } catch (IOException ex) {
                 System.out.println("EXCEPTION: " + ex.getMessage());
                 throw new UncheckedIOException(ex);
             }
-            System.out.println("Finished processing Path: " + path);
+            System.out.println("Finished processing Path: " + p);
         });
         metricsFiles.close();
-        Path p = Paths.get(PARENT_DIR + "/" + OUTPUT_FILENAME);
+        System.out.println("Size of output: " + output.length());
+        String dest = PARENT_DIR + "processed_metrics/" + OUTPUT_FILENAME ;
+        Path p = Paths.get(dest);
+        Files.createDirectory(Paths.get(PARENT_DIR + "processed_metrics/"));
+        Files.createFile(p);
         Files.write(p, output.toString().getBytes());
         return;
     }
@@ -76,8 +84,6 @@ public class MetricsProcessor {
     private static void processLine(String line, String taskId) throws UncheckedIOException {
 
         try {
-            String dest = PARENT_DIR + "processed_metrics/" ;
-
 
            if (line.contains("heapMemoryMax")) {
                 storeCpuAndMemory(g.fromJson(line, Map.class), taskId);
@@ -94,7 +100,7 @@ public class MetricsProcessor {
 
     public static void storeCpuAndMemory(Map<String, Object> metrics, String taskId) throws IOException {
         String transform_name                   = metrics.get("tag").toString();
-        String timestamp                        = metrics.get("epochMillis").toString();
+        Double timestamp_ms                      = Double.valueOf(metrics.get("epochMillis").toString());
         String system_cpu_usage                 = metrics.get("systemCpuLoad").toString();
         String process_cpu_usage                = metrics.get("processCpuLoad").toString();
         String nonheap_memory_in_use            = metrics.get("nonHeapMemoryTotalUsed").toString();
@@ -103,11 +109,11 @@ public class MetricsProcessor {
         String heap_memory_in_use               = metrics.get("heapMemoryTotalUsed").toString();
         String heapmax                          = metrics.get("heapMemoryMax").toString();
         String heap_memory_committed_in_jvm     = metrics.get("heapMemoryCommitted").toString();
-        String vmsize_incl_swap                 = metrics.get("vmSize").toString();
+        String vmsize_incl_swap                 = Long.toString(Math.round(Double.valueOf(metrics.get("vmSize").toString())));
         String host                             = metrics.get("name").toString();
         String role                             = metrics.get("role").toString();
         String processUUID                      = metrics.get("processUuid").toString();
-        String virt_mem_hwm                     = metrics.get("vmHWM").toString();
+        String virt_mem_hwm                     = Long.toString(Math.round(Double.valueOf(metrics.get("vmHWM").toString())));
 
         BigDecimal total_memory_in_use = new BigDecimal(heap_memory_in_use).add(new BigDecimal(nonheap_memory_in_use));
         BigDecimal total_memory_committed = new BigDecimal(heap_memory_committed_in_jvm).add(new BigDecimal(nonheap_memory_committed_in_jvm));
@@ -116,20 +122,20 @@ public class MetricsProcessor {
         String[] line = new String[CPU_AND_MEM_SCHEMA.size()];
 
         line[CPU_AND_MEM_SCHEMA.indexOf("transform_name")] = transform_name;
-        line[CPU_AND_MEM_SCHEMA.indexOf("timestamp_epoch")] = new BigDecimal(timestamp).divide(BigDecimal.valueOf(1000)).toString();
-        line[CPU_AND_MEM_SCHEMA.indexOf("timestamp_epoch_ms")] = new BigDecimal(timestamp).toString();
+        line[CPU_AND_MEM_SCHEMA.indexOf("timestamp_epoch")] = Long.toString(Math.round(timestamp_ms.doubleValue() / 1000.0));
+        line[CPU_AND_MEM_SCHEMA.indexOf("timestamp_epoch_ms")] = Long.toString(Math.round(timestamp_ms));
         line[CPU_AND_MEM_SCHEMA.indexOf("host")] = host;
         line[CPU_AND_MEM_SCHEMA.indexOf("role")] = role;
-        line[CPU_AND_MEM_SCHEMA.indexOf("executor_number")] = taskId.toString();
+        line[CPU_AND_MEM_SCHEMA.indexOf("executor_number")] = "";// we need to set this in the profiler code. we used to get this from dcos in a log file name. taskId.toString();
         line[CPU_AND_MEM_SCHEMA.indexOf("process_uuid")] = processUUID.toString();
         line[CPU_AND_MEM_SCHEMA.indexOf("infrastructure")] = "implementation2";
         line[CPU_AND_MEM_SCHEMA.indexOf("system_cpu_usage")] = new BigDecimal(system_cpu_usage).toString();
         line[CPU_AND_MEM_SCHEMA.indexOf("process_cpu_usage")] = new BigDecimal(process_cpu_usage).toString();
         line[CPU_AND_MEM_SCHEMA.indexOf("memory_in_use")] = total_memory_in_use.toString();
         line[CPU_AND_MEM_SCHEMA.indexOf("memory_committed")] = total_memory_committed.toString();
-        line[CPU_AND_MEM_SCHEMA.indexOf("virtmemsize_incl_swap")] = new BigDecimal(vmsize_incl_swap).toString();
-        line[CPU_AND_MEM_SCHEMA.indexOf("max_memory_available")] = total_memory_max.toString();
-        line[CPU_AND_MEM_SCHEMA.indexOf("virt_mem_hwm")] = virt_mem_hwm.toString();
+        line[CPU_AND_MEM_SCHEMA.indexOf("virtmemsize_incl_swap")] = vmsize_incl_swap;
+        line[CPU_AND_MEM_SCHEMA.indexOf("max_memory_available")] = Long.toString(total_memory_max.longValue());
+        line[CPU_AND_MEM_SCHEMA.indexOf("virt_mem_hwm")] = virt_mem_hwm;
         line[CPU_AND_MEM_SCHEMA.indexOf("mem_usage")] = total_mem_usage.toString();
 
         output.append(String.join(",", line));
@@ -157,3 +163,4 @@ public class MetricsProcessor {
 
     }
 }
+
