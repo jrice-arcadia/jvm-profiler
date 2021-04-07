a63ddac52fe2ad3324a545b72388ae1bceb79212
John Rice <john.rice@arcadia.io>
2021-04-06 21:14:03 -0400

    synchronize access

--- src/main/java/com/uber/profiling/reporters/S3OutputReporter.java
+++ src/main/java/com/uber/profiling/reporters/S3OutputReporter.java
@@ -13,40 +13,42 @@ import java.util.UUID;
 public class S3OutputReporter implements Reporter {
     private static AmazonS3 s3Client = new AmazonS3Client();
     private static UUID EXECUTOR_IDENTIFIER = UUID.randomUUID();
-    private static final int MAX_BUF_SIZE_BYTES = 5000000; // 5 mb -- make it much smaller for debug
+    private static final int MAX_BUF_SIZE_BYTES = 1000000; // 1 mb -- make it much smaller for debug
     //private static final int MAX_BUF_SIZE_BYTES = 500000; // 500kb //5kb - this happened SO fast. but it worked.
     private static StringBuffer buffer = new StringBuffer(MAX_BUF_SIZE_BYTES); // sb is threadsafe
 
     public void report(String profilerName, Map<String, Object> metrics) {
-        metrics.put("executor_number", EXECUTOR_IDENTIFIER.toString());
-        String json = JsonUtils.serialize(metrics);
-        buffer.append(profilerName + ":"+ json + "\n");
-        int length = buffer.length();
-        if (length < MAX_BUF_SIZE_BYTES) {
-            return;
-        }
-        try {
-            System.out.println("S3OutputReporter report() for " + profilerName + ". " +
-                    "Flushing metrics buffer to S3. Buffer length: " + length);
-            ObjectMetadata m = new ObjectMetadata();
-            String content = buffer.toString();
-            m.setContentLength(content.length());
-            m.setServerSideEncryption(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
-            System.out.println("Encryption set. Writing to S3.");
-            PutObjectResult res = s3Client.putObject("cigna.data.implementation2.arcadia",
-                    "cna004/sources/macig/instances/cna004/events/jvm_metrics_output/" + System.currentTimeMillis() + "_" + metrics.get("host") + ".txt",
-                    new ByteArrayInputStream(content.getBytes()),
-                    m);
-            System.out.println("S3 client returned from putObject(). Emptying buffer.");
-            /** Emptying the buffer is more performant than re-allocating, probably. **/
-            buffer.delete(0, buffer.length());
-        }  catch (AmazonS3Exception e) {
-            System.out.println("AmazonS3Exception thrown.");
-            System.out.println(e.getErrorCode());
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Exception thrown. Type: " + e.getClass());
-            System.out.println("message: " + e.getMessage());
+        synchronized (this) {
+            metrics.put("executor_number", EXECUTOR_IDENTIFIER.toString());
+            String json = JsonUtils.serialize(metrics);
+            buffer.append(profilerName + ":" + json + "\n");
+            int length = buffer.length();
+            if (length < MAX_BUF_SIZE_BYTES) {
+                return;
+            }
+            try {
+                System.out.println("S3OutputReporter report() for " + profilerName + ". " +
+                        "Flushing metrics buffer to S3. Buffer length: " + length);
+                ObjectMetadata m = new ObjectMetadata();
+                String content = buffer.toString();
+                m.setContentLength(content.length());
+                m.setServerSideEncryption(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
+                System.out.println("Encryption set. Writing to S3.");
+                PutObjectResult res = s3Client.putObject("cigna.data.implementation2.arcadia",
+                        "cna004/sources/macig/instances/cna004/events/jvm_metrics_output/" + System.currentTimeMillis() + "_" + metrics.get("host") + ".txt",
+                        new ByteArrayInputStream(content.getBytes()),
+                        m);
+                System.out.println("S3 client returned from putObject(). Emptying buffer.");
+                /** Emptying the buffer is more performant than re-allocating, probably. **/
+                buffer.delete(0, buffer.length());
+            } catch (AmazonS3Exception e) {
+                System.out.println("AmazonS3Exception thrown.");
+                System.out.println(e.getErrorCode());
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Exception thrown. Type: " + e.getClass());
+                System.out.println("message: " + e.getMessage());
+            }
         }
     }
 
